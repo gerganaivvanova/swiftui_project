67299c2930452cb22eb73052bb8df54e2a3810d4
Gergana Ivanova <gergana.ivvanova@gmail.com>
2023-05-06 14:33:41 +0200

    Initial Commit

---
+++ swiftui-project/swiftui_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_projectApp.swift
+//  swiftui-project
+//
+//  Created by user238749 on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
