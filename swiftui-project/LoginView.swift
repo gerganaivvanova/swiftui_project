5998141a4c45a259cc04c535bd6c82ee44521b07
Gergana Ivanova <gergana.ivvanova@gmail.com>
2023-05-08 11:46:24 +0200

    feat: create Product details page

--- swiftui-project/LoginView.swift
+++ swiftui-project/LoginView.swift
@@ -10,111 +10,113 @@ import SwiftUI
 struct LoginView: View {
     @State private var email = "";
     @State private var password = "";
-    @State private var isLoginValid = true;
+    @State private var isLoginValid = false;
     @State private var isPasswordVisible = false;
     @State private var isEmailValid = true;
     @State private var showModal = false;
-    @State private var path: [String] = [];
+    @State private var isActive = false;
     
     var body: some View {
-        VStack{
-            Image("Logo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 250, height: 160, alignment: .center)
-                .padding(.top, 50)
-            Spacer()
-            
-            Text("Log in")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(Color(UIColor(named: "TextColor")!))
-                .frame(maxWidth:.infinity, alignment: .leading)
-                .padding(.leading, 30)
-            
-            TextField("Email", text: $email, onEditingChanged: { isEditing in
-                if !isEditing {
-                    self.validateEmail()
-                }
-            })
-            .autocapitalization(.none)
-            .padding()
-            .background(Color.white)
-            .cornerRadius(10.0)
-            .padding(.horizontal,26)
-            .padding(.bottom,20)
-            
-            if !isEmailValid {
-                Text("Invalid email")
-                    .foregroundColor(.red)
+        NavigationView{
+            VStack{
+                Image("Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250, height: 160, alignment: .center)
+                    .padding(.top, 50)
+                    .padding(.bottom,50)
                 
-            }
-            HStack {
-                if isPasswordVisible{
-                    TextField("Password", text: $password)
-                        .autocapitalization(.none)
-                        .padding()
-                        .background(Color.white)
-                        .foregroundColor(isLoginValid ? .black : .red)
-                    
-                } else {
-                    SecureField("Password", text: $password)
-                        .autocapitalization(.none)
-                        .padding()
-                        .background(Color.white)
-                        .foregroundColor(isLoginValid ? .black : .red)
-                }
-                Button(action: {
-                    self.isPasswordVisible.toggle()
-                }) {
-                    Image(systemName: isPasswordVisible ? "eye.slash" : "eye")
-                        .foregroundColor(.gray)
-                }
-            }
-            .background(Color.white)
-            .cornerRadius(10.0)
-            .padding(.horizontal, 26)
+                Text("Log in")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(UIColor(named: "TextColor")!))
+                    .frame(maxWidth:.infinity, alignment: .leading)
+                    .padding(.leading, 30)
                 
-                Button(action: {
-                    self.validateLogin()
-                }) {
-                    Text("Log in")
-                        .foregroundColor(.white)
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 50)
-                        .background(isLoginValid ? Color.blue : Color.gray)
-                        .cornerRadius(5.0)
-                        .padding(.top, 50)
-                }
-                .disabled(email.isEmpty || !isEmailValid || password.isEmpty);           Spacer()
+                TextField("Email", text: $email)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10.0)
+                    .padding(.horizontal,26)
+                    .padding(.bottom,20)
+                    .onChange(of: email) {value in
+                        self.isEmailValid = validateEmail(value)
                     }
-        .frame(maxWidth: .infinity,maxHeight: .infinity)
-        .background(Image("Login Background")
-            .resizable()
-            .scaledToFill()
-            .edgesIgnoringSafeArea(.all)
-        )
-        .alert(isPresented: $showModal) {
-            Alert(title: Text("Login Failed"),
-                  message: Text("Your email or password is incorrect. Please try again."),
-                  dismissButton: .default(Text("OK")))
-        }    }
+                if !isEmailValid {
+                    Text("Invalid email")
+                        .foregroundColor(.red)
+                    
+                }
+                HStack {
+                    if isPasswordVisible{
+                        TextField("Password", text: $password)
+                            .autocapitalization(.none)
+                            .padding()
+                            .background(Color.white)
+                            .foregroundColor(isLoginValid ? .black : .red)
+                        
+                    } else {
+                        SecureField("Password", text: $password)
+                            .autocapitalization(.none)
+                            .padding()
+                            .background(Color.white)
+                    }
+                    Button(action: {
+                        self.isPasswordVisible.toggle()
+                    }) {
+                        Image(systemName: isPasswordVisible ? "eye.slash" : "eye")
+                            .foregroundColor(.gray)
+                    }
+                }
+                .background(Color.white)
+                .cornerRadius(10.0)
+                .padding(.horizontal, 26)
+               
+                NavigationLink(
+                    destination: ProductDetailsView(),
+                    isActive: $isActive,
+                    label: {
+                        Button(action: {
+                            self.validateLogin()
+                            
+                        }){
+                            Text("Log in")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal, 50)
+                                .background(isLoginValid ? Color.blue : Color.gray)
+                                .cornerRadius(5.0)
+                                .padding(.top, 50)
+                        }})
+                .disabled(email.isEmpty || !isEmailValid || password.isEmpty);
+            }
+            
+            .frame(maxWidth: .infinity,maxHeight: .infinity)
+            .background(Image("Login Background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            )
+            .alert(isPresented: $showModal) {
+                Alert(title: Text("Login Failed"),
+                      message: Text("Your email or password is incorrect. Please try again."),
+                      dismissButton: .default(Text("OK")))
+            }
+            
+        }
+    }
     
-    
-    private func validateEmail() {
+    private func validateEmail(_ email: String) -> Bool {
         let emailPredicate = NSPredicate(format:"SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}")
-        self.isEmailValid = emailPredicate.evaluate(with: self.email)
+        return emailPredicate.evaluate(with: email)
     }
     
     private func validateLogin() {
         // For demo purposes, hardcoded valid credentials are used.
         let validEmail = "user@example.com"
         let validPassword = "password"
-        
-        if email.isEmpty || !isEmailValid || password.isEmpty {
-            return
-        }
-        
+
         if email == validEmail && password == validPassword {
             // Login successful
             self.isLoginValid = true
---
+++ swiftui-project/ProductDetailsView.swift
@@ -0,0 +1,119 @@
+//
+//  HomePageView.swift
+//  swiftui-project
+//
+//  Created by user238749 on 5/7/23.
+//
+
+import SwiftUI
+
+struct ProductDetailsView: View {
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Button(action: {}, label: {
+                        Image(systemName: "chevron.backward")
+                            .font(.title2)
+                    })
+                    Spacer()
+                    Text("Product details")
+                        .font(.title3)
+                    Spacer()
+                    Button(action: {}, label: {
+                        Image(systemName: "cart.fill")
+                            .font(.title2)
+                            .foregroundColor(Color.black)
+                    })
+                }
+                .padding(.horizontal)
+                
+                ZStack(alignment: .topTrailing) {
+                    Image("ProductDetails")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 20)
+                    
+                    Button(action: {}, label: {
+                        HStack(spacing: 4) {
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(.green)
+                            Text("In stock")
+                                .foregroundColor(.black)
+                            
+                        }
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 10)
+                        .background(Color.white)
+                        .cornerRadius(26)
+                        .padding(.vertical,15)
+                        .padding(.horizontal, 30)
+                    })
+                }
+                
+                
+                VStack(alignment: .leading) {
+                    HStack(spacing:1){
+                        Text("Stargazer's Tea Set")
+                            .font(.title3)
+                            .bold()
+                        Spacer()
+                        Text("5")
+                            .font(.headline)
+                            .bold()
+                        Image(systemName: "star.fill")
+                            .font(.subheadline)
+                            .foregroundColor(Color(UIColor(named: "TextColor")!))
+                        Image(systemName: "star.fill")
+                            .font(.subheadline)
+                            .foregroundColor(Color(UIColor(named: "TextColor")!))
+                        Image(systemName: "star.fill")
+                            .font(.subheadline)
+                            .foregroundColor(Color(UIColor(named: "TextColor")!))
+                        Image(systemName: "star.fill")
+                            .font(.subheadline)
+                            .foregroundColor(Color(UIColor(named: "TextColor")!))
+                        Image(systemName: "star.fill")
+                            .font(.subheadline)
+                            .foregroundColor(Color(UIColor(named: "TextColor")!))
+                    }
+                    
+                    Text("Category: Home")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                    
+                    Text("Indulge in a heavenly tea experience with our Stargazer’s Tea Set, featuring a constellation-themed teapot and matching teacups. Crafted from fine porcelain, this elegant set will transport you to the cosmos with every sip.")
+                        .font(.body)
+                        .lineLimit(nil)
+                        .padding(.vertical)
+                        .frame(width: 300)
+                    
+                    Text("$90.00")
+                        .font(.title)
+                        .bold()
+                        .padding(.vertical)
+                }
+                .padding(.horizontal,20)
+                .padding(.bottom,50)
+                
+                Button(action: {}, label: {
+                    Text("Add to cart")
+                        .foregroundColor(.white)
+                        .frame(width: 340, height: 46)
+                        .background(Color(UIColor(named: "TextColor")!))
+                        .cornerRadius(26)
+                })
+                .padding(.horizontal,25)
+            }
+            .navigationBarHidden(true)
+            .navigationBarBackButtonHidden(true)
+        }
+    }
+}
+
+struct ProductDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailsView()
+    }
+}
